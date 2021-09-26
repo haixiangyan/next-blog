8168782e9094a4977baeb76cb36b778cf69a47e3
haixiangyan <haixiang6123@gmail.com>
2021-09-26 20:25:54 +0800

    feat: update home page

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import Head from "next/head";
+import Layout from "../../components/layout";
 
 function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -13,7 +14,7 @@ function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   )
 }
 
