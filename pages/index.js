47ca54f76fcbc9277725c35cfd0935e12a750575
haixiangyan <haixiang6123@gmail.com>
2021-09-27 10:45:40 +0800

    feat: add blogs

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import Head from "next/head";
 import utilStyles from '../styles/utils.module.css';
 import Layout, {siteTitle} from "../components/layout";
+import {getSortedPostsData} from "../lib/posts";
+import classNames from "classnames";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -15,6 +26,21 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+
+      <section className={classNames(utilStyles.headingMd, utilStyles.padding1px)}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({id, date, title}) => (
+            <li key={id} className={utilStyles.listItem}>
+              {title}
+              <br/>
+              {id}
+              <br/>
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
